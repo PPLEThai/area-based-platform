d96f2d03c02d8ede96a798a910405f79ed12f051
nitikornbunya <nitikorn.bunya@gmail.com>
2025-01-16 17:11:58 +0700

    change api base url

--- composables/useUrbanIssues.js
+++ composables/useUrbanIssues.js
@@ -5,7 +5,7 @@ export const useUrbanIssues = () => {
 
     const postUrbanIssue = async (payload) => {
         try {
-            const response = await $axios.post('api/urbanissues', payload);
+            const response = await $axios.post('/urbanissues', payload);
             return response.data; // Return the response data
         } catch (error) {
             throw error.response ? error.response.data : new Error(error.message); // Throw for better error handling
@@ -14,7 +14,7 @@ export const useUrbanIssues = () => {
 
     const getUrbanIssues = async (params = {}) => {
         try {
-            const response = await $axios.get('api/urbanissues', {
+            const response = await $axios.get('/urbanissues', {
                 params
             });
             return response.data;
@@ -25,7 +25,7 @@ export const useUrbanIssues = () => {
 
     const deleteUrbanIssue = async (id, email) => {
         try {
-            const response = await $axios.delete(`api/urbanissues/${id}`, {
+            const response = await $axios.delete(`/urbanissues/${id}`, {
                 data: {
                     email
                 }, // Send email in the request body
@@ -36,9 +36,24 @@ export const useUrbanIssues = () => {
         }
     };
 
+    const updateUrbanIssue = async (id, payload) => {
+        try {
+            const response = await $axios.patch(`/urbanissues/${id}`, payload, {
+                headers: {
+                    'Content-Type': 'application/json', // Ensure proper content type
+                },
+            });
+            return response.data; // Return the response data
+        } catch (error) {
+            // Throw error response data for better error handling
+            throw error.response ? error.response.data : new Error(error.message);
+        }
+    };
+
     return {
         postUrbanIssue,
         getUrbanIssues,
-        deleteUrbanIssue
+        deleteUrbanIssue,
+        updateUrbanIssue
     };
 };
